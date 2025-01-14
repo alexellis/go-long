2056d66814226a83aad1cae625a2c45baca48522
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2025-01-14 17:47:27 +0000

    Update py-long example
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- py-long/handler.py
+++ py-long/handler.py
@@ -7,12 +7,12 @@ def handle(event, context):
     s = os.getenv("handler_wait_duration_secs", "5")
     dur = int(s)
 
-    sys.stderr.write('>> handler waiting for {}\n'.format(s))
+    sys.stderr.write('>> handler waiting for {}s\n'.format(s))
     sys.stderr.flush()
 
     time.sleep(dur)
 
-    sys.stderr.write('<< handler waited for {}\n'.format(s))
+    sys.stderr.write('<< handler waited for {}s\n'.format(s))
     sys.stderr.flush()
 
     return {
